005cc042be3aa124eedf21ab60998e74adf86d52
gzqx <24262751+gzqx@users.noreply.github.com>
2025-03-19 14:01:42 +0100

    task_scheduler_init.h is deprecated and no longer exist. removing it and init line

--- src/tests/Nasri2019.cpp
+++ src/tests/Nasri2019.cpp
@@ -4,7 +4,7 @@
 #include "io.hpp"
 #include "global/space.hpp"
 
-#include "tbb/task_scheduler_init.h"
+//#include "tbb/task_scheduler_init.h"
 
 namespace dagSched{
 
@@ -12,7 +12,7 @@ bool test(std::istream &in,
 	std::istream &dag_in,
 	std::istream &aborts_in, const int m){
 
-    tbb::task_scheduler_init init(8);
+//    tbb::task_scheduler_init init(8);
 
     NP::Scheduling_problem<dtime_t> problem{
 		NP::parse_file<dtime_t>(in),
